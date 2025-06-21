4b8f60bc9122aa66e202b6f2540d81d0e569cdca
NikscodeView <nikunj02.rajput@gmail.com>
2025-06-21 08:01:29 -0400

    Created test for API to fetch vehicle information

--- test/e2e/test_e2e_user_flow.py
+++ test/e2e/test_e2e_user_flow.py
@@ -1,1 +1,18 @@
+def test_create_and_get_user(client):
+    
+    get_response = client.get("/getallmakes?format=json")
+    assert get_response.status_code == 200
+    m = get_response.json()
+    print(m)
+    print("Hello, world!")
+    
+    
+def test_create_and_post_user(client):
+    
+    print("Hello, world!")            
+    
+    get_responsepost = client.get("/getallmanufacturers?format=json")
+    assert get_responsepost.status_code == 200
+   
+
 

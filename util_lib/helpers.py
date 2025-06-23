faad188b579a1b8ba3b216900b35e978ba193fa2
NikscodeView <nikunj02.rajput@gmail.com>
2025-06-23 16:19:09 -0400

    Added function to create name and emails to create random user

--- util_lib/helpers.py
+++ util_lib/helpers.py
@@ -1,1 +1,8 @@
+import random
+import string
 
+def generate_email():
+    return f"user_{random.randint(1000,9999)}@example.com"
+
+def generate_name(length=6):
+    return ''.join(random.choices(string.ascii_letters, k=length))

43d2c9faddc7613c6d1ee28eb008a6381ed7b541
NikscodeView <nikunj02.rajput@gmail.com>
2025-06-26 15:28:40 -0400

    Create test_unit_user_api.py

---
+++ tests/unit/test_unit_user_api.py
@@ -0,0 +1,23 @@
+from unittest.mock import patch, Mock
+
+def test_user_creation_mock(client):
+    with patch("utils.api_client.requests.post") as mock_post:
+        mock_post.return_value.status_code = 201
+        mock_post.return_value.json.return_value = {"id": 5, "name": "Testing"}
+        response = client.post("/getallmanufacturers?format=json", json={"name": "Testing"})
+        assert response.status_code == 201
+        assert response.json()["name"] == "Testing"
+
+
+def test_get_user_from_service(client):
+    # Arrange
+    with patch("utils.api_client.requests.get") as mock_get:
+        mock_get.return_value.status_code = 200
+        mock_get.return_value.json.return_value = {"first_name": "Alice", "last_name": "Smith"}
+        response = client.get("/getallmanufacturers?format=json")
+        assert response.status_code == 200
+        assert response.json()['first_name'] == "Alice"
+        assert response.json()['last_name'] == "Smith"
+
+    
+    

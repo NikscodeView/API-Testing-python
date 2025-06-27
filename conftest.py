4947f9f766e4b717863618529aea31b85f47f433
NikscodeView <nikunj02.rajput@gmail.com>
2025-06-27 07:19:07 -0400

    Added object client information

--- conftest.py
+++ conftest.py
@@ -4,10 +4,18 @@ from utils.api_client import APIClient
 @pytest.fixture(scope="session")
 def base_url():
     return "https://vpic.nhtsa.dot.gov/api/vehicles"
-
+    
+@pytest.fixture(scope="session")
+def base_url_objects():
+    return "https://api.restful-api.dev"
+    
 @pytest.fixture(scope="session")
 def client(base_url):
     return APIClient(base_url)
+    
+@pytest.fixture(scope="session")
+def client_object(base_url_objects):
+    return APIClient(base_url_objects)
 
 @pytest.fixture(scope="function")
 def auth_headers():
